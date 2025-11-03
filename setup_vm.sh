378a8c68e3e73026fdc21713689fd3445d0245e5
Frederic-Rodrigues <58865527+Frederic-Rodrigues@users.noreply.github.com>
2025-11-03 13:48:27 +0000

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -134,12 +134,16 @@ sudo snap install zaproxy --classic
 
 # Install Gemini
 echo "Installing Gemini ..."
+cat >gemini.sh <<EOT
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 20
 nvm use 20
 npm install -g @google/gemini-cli
+EOT
+chmod +x gemini.sh
+sudo -u ${CHROME_REMOTE_USER_NAME} bash gemini.sh
 
 # Install VsCode
 echo "Installing VsCode..."
